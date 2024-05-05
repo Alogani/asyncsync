ff5170f689f30bec1f07640e05da77cfa86b1cab
alogani <alogani@mailfence.com>
2024-05-05 10:56:10 +0200

    Update docs

--- htmldocs/asyncsync.nim
+++ htmldocs/asyncsync.nim
@@ -2,6 +2,6 @@ import ../src/asyncsync
 import ../src/asyncsync/container
 import ../src/asyncsync/event
 import ../src/asyncsync/exports/fututils
-import ../src/asyncsync/listener
 import ../src/asyncsync/lock
+import ../src/asyncsync/osevents
 import ../src/asyncsync/semaphore
