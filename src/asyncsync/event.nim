cff93ed3986d49bad9391cf7920348667a37251c
alogani <alogani@mailfence.com>
2024-05-06 19:23:28 +0200

    Modify clear behaviour on events to make it more explicit, and add consume proc

--- src/asyncsync/event.nim
+++ src/asyncsync/event.nim
@@ -6,38 +6,43 @@ privateAccess(FutureBase)
 
 
 type
-    Event* = distinct Future[void]
+    Event* = ref object
+        fut: Future[void]
         ## Waiters wakening can be unordered (According to asyncdispatch doc: in the order of await macro call)
         ## For ordered events, cleaner method is to use in combination with a lock
 
 
 template await*(self: Event) =
     ## if called after trigger, ensure all preceding waiters have complete
-    await self.wait()
+    await self.fut()
 
 proc clean*[T](fut: Future[T]) =
     fut.finished = false
     fut.error = nil
 
 proc clear*(self: Event) =
-    if Future[void](self).finished:
-        Future[void](self).clean()
+    ## Clear guaranties that all waiters will be triggered, but no that have all waiters are already triggered
+    ## To be sure that all waiters will be triggered, await the event yourself, and you will be the last one
+    self.fut = newFuture[void]()
+
+proc consume*(self: Event) =
+    ## Waiters after this call will be triggered.
+    ## However it should be called immediatly without using another await (or then), or waiters will certainly will be all triggered
+    if self.fut.finished:
+        self.fut.clean()
 
 proc new*(T: type Event): T =
-    T(newFuture[void]("Event"))
+    T(fut: newFuture[void]())
 
 proc trigger*(self: Event) =
-    if not Future[void](self).finished:
-        Future[void](self).complete()
+    if not self.fut.finished:
+        self.fut.complete()
 
 proc triggered*(self: Event): bool =
-    Future[void](self).finished
-
-proc wait*(self: Event): Future[void] =
-    Future[void](self)
+    self.fut.finished
 
 converter toBool*(self: Event): bool =
-    Future[void](self).finished
+    self.fut.finished
 
 converter toFut*(self: Event): Future[void] =
-    self.wait()
+    self.fut
